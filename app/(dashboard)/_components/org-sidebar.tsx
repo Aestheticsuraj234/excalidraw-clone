6030dbb860a43310ffce07379048a19ed2655805
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-02-14 01:21:22 +0530

    Update layout and remove unused page component

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react'
+
+const OrgSidebar = () => {
+  return (
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5">OrgSidebar</div>
+  )
+}
+
+export default OrgSidebar
