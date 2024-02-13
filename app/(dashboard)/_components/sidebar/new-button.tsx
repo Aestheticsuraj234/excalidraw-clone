f5b7da96aaf7ce02e2fe24e3932dd4e6107a9ca9
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-02-14 02:29:32 +0530

    Add List component to sidebar and update next.config.js

--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -4,15 +4,23 @@ import { CreateOrganization } from "@clerk/nextjs";
 
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
+import { Hint } from "@/components/hint";
 
 export const NewButton = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
         <div className="aspect-square">
+          <Hint
+          label="Create new organization"
+          side="right"
+          align="start"
+          sideOffset={18}
+          >
           <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60">
             <Plus className="text-white" />
           </button>
+          </Hint>
         </div>
       </DialogTrigger>
 
