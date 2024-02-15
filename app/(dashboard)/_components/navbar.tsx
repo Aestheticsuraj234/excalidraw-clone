d3464b25f87d10f99e36e039cce6313fc95c45ad
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-02-15 11:43:26 +0530

    Refactor navbar component and add invite button

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,41 +1,47 @@
 "use client";
-import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
-import React from 'react'
-import { SearchInput } from './search-input';
+import {
+  OrganizationSwitcher,
+  UserButton,
+  useOrganization,
+} from "@clerk/nextjs";
+import React from "react";
+import { SearchInput } from "./search-input";
+import { InviteButton } from "./invite-button";
 
 const Navbar = () => {
+  const { organization } = useOrganization();
   return (
     <div className="flex items-center gap-x-4 p-5 ">
-        <div className="hidden lg:flex lg:flex-1">
+      <div className="hidden lg:flex lg:flex-1">
         <SearchInput />
-        </div>
-        <div className="block lg:hidden flex-1">
+      </div>
+      <div className="block lg:hidden flex-1">
         <OrganizationSwitcher
-        hidePersonal
-        appearance={{
-          elements: {
-            rootBox: {
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              width: "100%",
-              maxWidth: "376px",
+          hidePersonal
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                maxWidth: "376px",
+              },
+              organizationSwitcherTrigger: {
+                padding: "6px",
+                width: "100%",
+                borderRadius: "8px",
+                border: "1px solid #E5E7EB",
+              },
             },
-            organizationSwitcherTrigger: {
-              padding: "6px",
-              width: "100%",
-              borderRadius: "8px",
-              border: "1px solid #E5E7EB",
-            },
-          },
-        }}
-      />
-        </div>
-       
-        <UserButton />
-    
-    </div>
-  )
-}
+          }}
+        />
+      </div>
+      {organization && <InviteButton />}
 
-export default Navbar
+      <UserButton />
+    </div>
+  );
+};
+
+export default Navbar;
