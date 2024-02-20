f9546dc93c4f2cd896318583eeece3448b5d8143
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-02-20 14:05:31 +0530

    Update code formatting and fix minor bugs

---
+++ app/board/[boardId]/_components/canvas.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Info } from "./info";
+import { Participants } from "./participants";
+import Toolbar from "./toolbar";
+
+
+export const Canvas = ()=>{
+    return(
+        <main className="h-full w-full relative bg-netural-100 touch-none">
+            <Info/>
+            <Participants/>
+            <Toolbar/>
+        </main>
+    )
+}
